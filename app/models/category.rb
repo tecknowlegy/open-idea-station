de1848174462c0149281e07ec3f7cd1041bb23b9
dinobi <dino.kennetcorp@gmail.com>
2018-02-05 21:41:50 +0100

    ch-clean-implemented-functionalities

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :idea_categories
   has_many :ideas, through: :idea_categories
+  validates_uniqueness_of :name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Idea.create(
-            name: "Open Idea Station",
-            description: "This is the OIS base application built with Ruby on Rails",
-            author: "Dinobi",
-            organization: "OIS"
-            )
 
-Idea.create(
-            name: "Quadnate",
-            description: "4 spaces of cordination",
-            author: "Dinobi",
-            organization: "Quadnate"
-            )
+ideas_list = [
+  [ "Open Idea Station", "This is the OIS base application built with Ruby on Rails", "Dinobi", "OIS" ],
+  [ "Quadnate", "4 spaces of cordination", "Dinobi", "OIS" ],
+  [ "Happy Helpers", "A code description of how methods should be named in ruby", "Dinobi", "OIS" ],
+  [ "PostIt", "A group messaging app", "Dinobi", "Andela" ],
+  [ "PosticoDB", "An open source database system", "Adam", "Postico" ],
+  [ "Paale", "Connect with loved ones", "Philip", "PJN-LLC" ],
+  [ "Cookerel", "Food as a service", "Dinobi", "OIS" ],
+  [ "SportyVite", "Know what your favourite sportman is listening to in realtime", "Dinobi", "OIS" ],
+
+]
+
+ideas_list.each do |name, description, author, organization|
+  Idea.create(
+                  name: name,
+                  description: description,
+                  author: author,
+                  organization: organization
+                )
+end
+
+
+# Idea.create(
+#             name: "Open Idea Station",
+#             description: "This is the OIS base application built with Ruby on Rails",
+#             author: "Dinobi",
+#             organization: "OIS"
+#             )
+
+# Idea.create(
+#             name: "Quadnate",
+#             description: "4 spaces of cordination",
+#             author: "Dinobi",
+#             organization: "Quadnate"
+#             )
 
 User.create(
             username: "Dinobi",
