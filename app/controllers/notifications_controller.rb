2150ecc515573d552b7a5c4ea7f518afb27aa4ef
dinobi <dino.kennetcorp@gmail.com>
2018-04-21 00:07:18 +0100

    ch(deployV1): add first app release
    [Delivers #156967682]

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
   before_action :set_notification
 
   private
-    def :set_notification
+    def set_notification
       @notifications = Notification.where(recipient: current_user).unread
     end
 end
