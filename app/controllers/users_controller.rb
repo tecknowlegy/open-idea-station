d9d9810a316d2a8eebe8ac151314688bcfc07d16
dinobi <dino.kennetcorp@gmail.com>
2019-02-17 03:56:41 +0100

    update user controller to handle omniauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize, only: %i[new create]
+  skip_before_action :authorize, only: %i[new create create_omniauth_session omniauth_user]
 
   # GET /users/new
   def new
@@ -9,32 +9,36 @@ class UsersController < ApplicationController
   # POST /users
   def create
     user = User.new(user_params)
-    user_session = session_params.merge(
-      { username: user_params[:username], password: user_params[:password] }
-    )
+    auth_params = { username: user_params[:username], password: user_params[:password] }
+
+    if user.save
+      # TODO: Translations << Add to translations
+      flash[:notice] = "Your account was successfully created"
+      create_session(auth_params)
+    else
+      flash[:error] = user.errors.full_messages[0]
+      render :new
+    end
+  end
+
+  def create_omniauth_session
+    auth_params = { username: user_params[:username], password: user_params[:password] }
+
+    create_session(auth_params)
+  end
+
+  def omniauth_user
+    @omniauth_user = Acorn::OmniauthService.new(omniauth_hash: omniauth_hash).call
+
     respond_to do |format|
-      if user.save
-        # TODO: Translations << Add to translations
-        flash[:notice] = "Your account was successfully created"
-        auth_hash = Acorn::AuthenticateUserService.call(user_session)
-
-        if auth_hash.success?
-          cookies[:user_id] = session["token"] = auth_hash.result
-          format.html do
-            redirect_to ideas_path
-          end
-        else
-          # TODO: create job that deletes from the database, resources
-          # that have incomplete instanciated columns present
-          flash[:notice] = "We could not create your session at this time. Please login to continue"
-          format.html do
-            redirect_to index_path
-          end
+      if @omniauth_user.success?
+        case @omniauth_user.result[:path]
+        when "user" then format.html { render :omniauth_user }
+        when "session" then format.html { render :omniauth_session }
         end
-
       else
-        flash[:error] = user.errors.full_messages[0]
-        format.html { render :new }
+        flash[:error] = @omniauth_user.errors[:omniauth_error].first
+        render :new
       end
     end
   end
@@ -42,6 +46,30 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :username, :email, :password, :password_confirmation,
+      :provider, :picture, :bio
+    )
+  end
+
+  def omniauth_hash
+    request.env["omniauth.auth"]
+  end
+
+  def create_session(auth_params)
+    auth_token = Acorn::AuthenticateUserService.call(auth_params)
+
+    respond_to do |format|
+      if auth_token.success?
+        # TODO: Translations << Add to translations
+        flash[:success] = "You are now signed in"
+        cookies[:user_id] = session["token"] = auth_token.result
+        format.html { redirect_to "/ideas" }
+      else
+        # << and this
+        flash[:error] = auth_token.errors[:user_authentication].first
+        format.html { redirect_to new_session_path }
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,39 +9,12 @@ class User < ApplicationRecord
   validates_presence_of :email, :password_digest
   validates :email, uniqueness: { case_sensitive: false }, format: EMAIL_REGEX
 
-  validates :password, presence: true
+  validates :password, presence: true, if: :password
   validates :password, confirmation: { case_sensitive: true }
 
   has_many :ideas, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, foreign_key: :recipient_id, dependent: :destroy
-
-  def self.find_or_create_from_omniauth(auth)
-    case auth.provider
-    when "google_oauth2"
-      find_params = { provider: auth.provider, uid: auth.uid }
-      where(find_params).first_or_initialize.tap do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.username = auth.info.first_name unless auth.info.first_name.nil?
-        user.email = auth.info.email
-        user.password = auth.credentials.token[-15, 15]
-        user.picture = auth.info.image
-        user.save
-      end
-    when "github"
-      find_params = { provider: auth.provider, uid: auth.uid }
-      where(find_params).first_or_initialize.tap do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.username = auth.info.name unless auth.info.name.nil?
-        user.email = auth.info.email unless auth.info.email.nil?
-        user.password = auth.credentials.token[-15, 15]
-        user.picture = auth.extra["raw_info"].avatar_url unless auth.extra["raw_info"]&.avatar_url.nil?
-        user.save
-      end
-    end
-  end
 
   def self.find_user_by_session(token)
     session = Session.find_by(token: token, active: true)
@@ -50,6 +23,10 @@ class User < ApplicationRecord
 
   def bio
     read_attribute(:bio) || "Bio is yet to be updated"
+  end
+
+  def provider
+    read_attribute(:provider) || "open-idea-stattion"
   end
 
   concerning :Sessions do
