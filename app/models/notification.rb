d0aa047be1a3bbd07905887eed6ac9bee58b0639
dinobi <dino.kennetcorp@gmail.com>
2019-02-21 15:59:30 +0100

    add scopes to noitification model
    - change is_read column to read_at for notifications
    - add recent notification scope to notifications
    - update unread scope for notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,10 @@
 class Notification < ApplicationRecord
+  SIZE = 5
+
   belongs_to :recipient, class_name: "User"
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
 
-  scope :unread, -> { where(is_read: false) }
+  scope :unread, -> { where(read_at: nil) }
+  scope :recent, ->(size) { limit(size || SIZE) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190210103105) do
+ActiveRecord::Schema.define(version: 20190221131927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 20190210103105) do
   create_table "notifications", force: :cascade do |t|
     t.integer "recipient_id"
     t.integer "actor_id"
-    t.boolean "is_read"
+    t.boolean "read_at"
     t.string "action"
     t.string "notifiable_type"
     t.bigint "notifiable_id"
