0548b2e3501cc9eea2d48deafbea399739e5a15b
dinobi <dino.kennetcorp@gmail.com>
2019-03-02 09:36:09 +0100

    build email_confirmation template and english locale translations

---
+++ app/mailers/users_mailer.rb
@@ -0,0 +1,11 @@
+class UsersMailer < ApplicationMailer
+
+  def email_confirmation(user_id, token)
+    @user = User.find_by(id: user_id)
+    @token = token
+
+    send_mail_with_user_locale(@user.locale) do
+      mail to: @user.email, subject: subject
+    end
+  end
+end
