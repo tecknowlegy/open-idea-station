b26a5a2808f9bf443dd1c4874639d66811c278a2
dinobi <dino.kennetcorp@gmail.com>
2019-03-21 23:29:58 +0100

    add mailer method for reset password

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -7,4 +7,13 @@ class UsersMailer < ApplicationMailer
       mail to: "#{@user.username} <#{@user.email}>", subject: subject
     end
   end
+
+  def reset_password(user_id, token)
+    @user = User.find_by(id: user_id)
+    @token = token
+
+    send_mail_with_user_locale(@user.locale) do
+      mail to: "#{@user.username} <#{@user.email}>", subject: subject
+    end
+  end
 end
