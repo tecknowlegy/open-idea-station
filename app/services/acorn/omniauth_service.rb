0326b470f090fbf9955c5fdad27fa179cbef09a7
dinobi <dino.kennetcorp@gmail.com>
2019-06-26 12:42:30 +0100

    refactor omniauth user service

--- app/services/acorn/omniauth_service.rb
+++ app/services/acorn/omniauth_service.rb
@@ -17,38 +17,46 @@ class Acorn::OmniauthService
     if omniauth_hash.present?
       user = User.find_by(email: omniauth_hash.info.email)
 
-      # if user is found then we should include/update
-      # certain attributes and use session flow
       if user.present?
-        user.update(
-          provider: omniauth_hash.provider || user.provider,
-          picture: omniauth_hash.extra["raw_info"]&.avatar_url || omniauth_hash.info.image
-        )
-        return [user, "session"]
+        returning_user
+      else
+        new_user
       end
-
-      # else use normal create flow
-      user =  case omniauth_hash.provider
-              when "google_oauth2"
-                User.new do |u|
-                  u.provider = omniauth_hash.provider
-                  u.username = omniauth_hash.info.first_name unless omniauth_hash.info.first_name.nil?
-                  u.email    = omniauth_hash.info.email
-                  u.picture  = omniauth_hash.info.image
-                end
-              when "github"
-                User.new do |u|
-                  u.provider = omniauth_hash.provider
-                  u.username = omniauth_hash.extra["raw_info"].login unless omniauth_hash.extra["raw_info"]&.login.nil?
-                  u.email    = omniauth_hash.extra["raw_info"].email unless omniauth_hash.extra["raw_info"]&.email.nil?
-                  u.picture  = omniauth_hash.extra["raw_info"].avatar_url unless omniauth_hash.extra["raw_info"]&.avatar_url.nil?
-                end
-              end
-      [user, "new"]
     else
       errors.add(:omniauth_error, "User credential was not provided by external service")
     end
   rescue StandardError
     errors.add(:omniauth_error, "An error occurred when trying to read omniuth data")
+  end
+
+  def returning_user
+    user.update(
+      provider: omniauth_hash.provider || user.provider,
+      picture: omniauth_hash.extra["raw_info"]&.avatar_url || omniauth_hash.info.image
+    )
+
+    [user, "session"]
+  end
+
+  def new_user
+    # else use normal create flow
+    user =  case omniauth_hash.provider
+            when "google_oauth2"
+              User.new do |u|
+                u.provider = omniauth_hash.provider
+                u.username = omniauth_hash.info.first_name unless omniauth_hash.info.first_name.nil?
+                u.email    = omniauth_hash.info.email
+                u.picture  = omniauth_hash.info.image
+              end
+            when "github"
+              User.new do |u|
+                u.provider = omniauth_hash.provider
+                u.username = omniauth_hash.extra["raw_info"].login unless omniauth_hash.extra["raw_info"]&.login.nil?
+                u.email    = omniauth_hash.extra["raw_info"].email unless omniauth_hash.extra["raw_info"]&.email.nil?
+                u.picture  = omniauth_hash.extra["raw_info"].avatar_url unless omniauth_hash.extra["raw_info"]&.avatar_url.nil?
+              end
+            end
+
+    [user, "new"]
   end
 end
